c708ab1faf995e949a65ce579c63e23ee5af62c8
Ian <ian.bulmer89@gmail.com>
2015-10-05 08:00:25 -0400

    added a clock

--- standup30min.js
+++ standup30min.js
@@ -1,9 +1,23 @@
-function start(){
-  document.getElementById("clicked").innerHTML = "timer started"; setInterval(remind, 1800000);
+var current = Date.parse(new Date());
+var future = current+toMills(30);
+setInterval(myTimer, 1000);
+
+function toMins(mills){
+  return mills/1000/60;
 }
 
-function remind() {
-	document.getElementById("clicked").innerHTML = "timer started";
-    alert('Stand Up');
+
+function toMills(mins){
+  return mins*60*1000;
 }
 
+function myTimer(){
+  current = Date.parse(new Date());
+  if(future-current>=0){
+    document.getElementById("clock").innerHTML = future-current;
+  } else {
+  	alert("stand up");
+  	current=Date.parse(new Date());
+  	future=current+toMills(30);
+  }
+}
