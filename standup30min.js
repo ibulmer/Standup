bdd8a31467a7d63883dd803667e37fefd93e979d
Ian <ian.bulmer89@gmail.com>
2015-10-05 22:55:56 -0400

    Converted time to hh:mm format

--- standup30min.js
+++ standup30min.js
@@ -6,7 +6,6 @@ function toMins(mills){
   return mills/1000/60;
 }
 
-
 function toMills(mins){
   return mins*60*1000;
 }
@@ -14,7 +13,17 @@ function toMills(mins){
 function myTimer(){
   current = Date.parse(new Date());
   if(future-current>=0){
-    document.getElementById("clock").innerHTML = future-current;
+  	var time = (future-current)/1000;
+  	var minutes = Math.floor(time/60);
+  	var seconds = Math.floor(time%60);
+  	minutes=minutes.toString();
+  	if (seconds<10){
+  		seconds = "0" + seconds.toString();
+  	} else {
+  		seconds = seconds.toString();
+  	}
+  	time = minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = time;
   } else {
   	alert("stand up");
   	current=Date.parse(new Date());
